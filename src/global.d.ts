f5ece2b6aa2c81f3774bff501f161c9860d43878
Jenny Xing <jenny.xing@nytimes.com>
2021-03-19 14:34:20 -0400

    exploring open interfaces

---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+// whatever you declare here they'll end up being additional globals.
+// only type information exists in this file.
+// declare const googleAnalytics: {} ; //an example
+
+// what to do if @types/react-test-renderer didn't exist as a package
+// declare module 'react-test-renderer' {
+//     export function create(...args: any[]): any;
+// }
--- tests/components/ChannelFooter.test.tsx
+++ tests/components/ChannelFooter.test.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import Footer from '../../src/ui/components/Channel/Footer';
 
+// // you can declare additional things here. it'll merge with what @types/react-test-renderer already provided.
+// declare module 'react-test-renderer' {
+//   interface TestRendererOptions {
+//     company: string;
+//   }
+// }
+
 test('Link changes the class when hovered', () => {
   const component = renderer.create(
     <Footer channel={{ name: 'recruiting' }} />,
